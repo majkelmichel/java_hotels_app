a7613fb45365107bb4a2179ec09539711d90835c
majkelmichel <mich.zaw2002@gmail.com>
2021-12-16 14:47:36 +0100

    Poprawiona konfiguracja Swaggera

--- src/main/java/pl/michalzawadzki/hotelsapplication/remote/config/SwaggerConfiguration.java
+++ src/main/java/pl/michalzawadzki/hotelsapplication/remote/config/SwaggerConfiguration.java
@@ -18,11 +18,11 @@ public class SwaggerConfiguration {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("pl.kinga.pizza"))
+                .apis(RequestHandlerSelectors.basePackage("pl.michalzawadzki.hotelsapplication"))
                 .build()
                 .useDefaultResponseMessages(false)
                 .ignoredParameterTypes()
-                .apiInfo(new ApiInfo("Pizza information", "","","",new Contact("","","")
+                .apiInfo(new ApiInfo("Hotels information", "","","",new Contact("","","")
                         ,"","", Collections.emptyList()));
     }
 }
